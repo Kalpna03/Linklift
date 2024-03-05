09530281fce18fd2852d75480543af79cfe3dd28
Kalpna03 <139703730+Kalpna03@users.noreply.github.com>
2024-03-05 21:07:51 +0530

    Update RideOptions.js

--- src/components/RideOptions.js
+++ src/components/RideOptions.js
@@ -1,15 +1,15 @@
 import React from "react";
+import RideOptionCard from "./RideOptionCard";
 
 function RideOptions({ rideOptions, onSelectRideOption }) {
   return (
     <div className="ride-options-container">
       {rideOptions.map((rideOption) => (
-        <div key={rideOption.vehicleNumber} className="ride-option">
-          <img src={rideOption.imageSrc} alt={rideOption.vehicleNumber} className="ride-option-image" />
-          <h3>{rideOption.vehicleInfo}</h3>
-          <p>Amount: {rideOption.amount}</p>
-          <button onClick={() => onSelectRideOption(rideOption)}>Select Ride</button>
-        </div>
+        <RideOptionCard
+          key={rideOption.vehicleNumber}
+          rideOption={rideOption}
+          onSelectRideOption={onSelectRideOption}
+        />
       ))}
     </div>
   );
